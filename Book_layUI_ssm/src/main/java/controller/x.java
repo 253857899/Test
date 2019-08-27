dc7a43ada08aae26619367372755ad27ea9eaf3f
Cursh <Cursh@DESKTOP-N810OSP.lan>
2019-08-27 11:30:34 +0800

    SAS

--- Book_layUI_ssm/src/main/java/controller/x.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-public class x {
-		public static void main(String[] args) {
-			System.out.println("xx");
-		}
-	}
-
--- Book_layUI_ssm/src/main/java/controller/xx.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-public class xx {
- public static void main(String[] args) {
-	System.out.println("name");
-	System.out.println("??");
-}
-}
