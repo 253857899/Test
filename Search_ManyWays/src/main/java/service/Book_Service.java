99256d99a8eaf812cea341a75b2c2bf914ab6cbe
Cursh <Cursh@DESKTOP-N810OSP.lan>
2019-08-28 20:17:05 +0800

    Search

---
+++ Search_ManyWays/src/main/java/service/Book_Service.java
@@ -0,0 +1,18 @@
+package service;
+
+
+import entity.Book;
+import utils.ReturnInfo;
+
+public interface Book_Service {
+	
+	public ReturnInfo select(String txt,Integer page,Integer limit);
+	
+	public Book selectById(int id);
+	
+	public void insert(Book t);
+	
+	public void update(Book t);
+	
+	public void delete(int id);
+}
---
+++ Search_ManyWays/src/main/java/service/Type_Service.java
@@ -0,0 +1,19 @@
+package service;
+
+
+
+import entity.Type;
+import utils.ReturnInfo;
+
+public interface Type_Service {
+	
+	public ReturnInfo select(String txt,Integer page,Integer limit);
+	
+	public Type selectById(int id);
+	
+	public void insert(Type t);
+	
+	public void update(Type t);
+	
+	public void delete(int id);
+}
